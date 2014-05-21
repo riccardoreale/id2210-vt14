720fcdc51db38cd86e943bdf99539eb785bd774d
Giovanni Simoni <simgidacav@gmail.com>
2014-05-21 19:28:06 +0200

    Moved all to Task.java

--- common/src/main/java/common/peer/Task.java
+++ common/src/main/java/common/peer/Task.java
@@ -6,6 +6,9 @@ public class Task {
 	protected final int numCpus;
 	protected final int memoryInMbs;
 	protected final int timeToHoldResource;
+	protected long enqueueTime; 
+	protected long allocateTime;
+	protected long deallocateTime;
 
 	public Task(long id, int numCpus, int memoryInMbs, int timeToHoldResource) {
 		super();
@@ -13,6 +16,25 @@ public class Task {
 		this.numCpus = numCpus;
 		this.memoryInMbs = memoryInMbs;
 		this.timeToHoldResource = timeToHoldResource;
+	    this.enqueueTime = 0; 
+	    this.allocateTime = 0;
+	    this.deallocateTime = 0;
+	}
+	
+	public void allocate () {
+		this.allocateTime = System.currentTimeMillis();
+	}
+	
+	public void deallocate () {
+		this.deallocateTime = System.currentTimeMillis();
+	}
+	
+	public long getQueueTime() {
+		return allocateTime - enqueueTime;
+	}
+	
+	public long getTotalTime() {
+		return deallocateTime - enqueueTime;
 	}
 
 	public long getId() {
