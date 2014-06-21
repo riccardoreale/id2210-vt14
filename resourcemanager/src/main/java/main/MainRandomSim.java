70366e844b366c42bcf903f466c6906909dc349e
Riccardo Reale <riccardo.reale@peerialism.com>
2014-06-21 21:24:16 +0200

    added initial figures

---
+++ resourcemanager/src/main/java/main/MainRandomSim.java
@@ -0,0 +1,86 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class MainRandomSim {
+
+	private final static boolean USE_ORACLE = true;
+	private final static int PROBES = 2;
+
+	private final static int numSeeds = 5;
+	private final static int[] nodes = new int[] { //
+//	0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 
+//	0.9, 0.95, 1
+	15, 20, 25, 30, 35, 40
+	// 0.8, 0.85, 0.9, 0.95, 1
+	};
+
+	public static void main(String[] args) throws Throwable {
+
+		cleanUp();
+
+		Long[] seeds = new Long[numSeeds];
+		for (int i = 1; i <= numSeeds; i++) {
+			seeds[i - 1] = (long) (44444L / i);
+		}
+		for (int numNodes : nodes) {
+			for (Long seed : seeds) {
+				MainRandom.main(new String[] { String.valueOf(USE_ORACLE), "" + seed,
+						"" + numNodes, String.valueOf(PROBES) });
+			}
+
+			averageData();
+		}
+
+	}
+
+	private static void cleanUp() {
+		try {
+
+			File f = new File("out.txt");
+			f.delete();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+
+	private static void averageData() {
+		try {
+
+			File f = new File("out.txt");
+			FileInputStream fstream = new FileInputStream(f);
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+			String strLine;
+
+			String load = null;
+			String probes = null;
+			double totAverage = 0;
+			double totPercentile = 0;
+			int lines = 0;
+			// Read File Line By Line
+			while ((strLine = br.readLine()) != null) {
+				lines++;
+				String[] split = strLine.split("\t");
+				load = split[0];
+				probes = split[1];
+				totAverage += Double.parseDouble(split[2]);
+				totPercentile += Double.parseDouble(split[3]);
+			}
+			double averageAverage = Math.round(totAverage / lines);
+			double averagePercentile = Math.round(totPercentile / lines);
+			System.err.println(load + "\t" + probes + "\t" + averageAverage
+					+ "\t" + averagePercentile);
+			// Close the input stream
+			in.close();
+			f.delete();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
+}
